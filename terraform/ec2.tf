ac3d3d82ab38db3df85f9ae257d782f08c9a76df
a4t <iwanomoto@gmail.com>
2019-11-29 11:06:03 +0900

    2台で比較するように変更

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,12 +3,13 @@ resource "aws_key_pair" "key" {
   public_key = file("/root/.ssh/id_rsa.pub")
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "public_app" {
   ami                  = local.ami_id
   instance_type        = local.instance_type
-  subnet_id            = aws_subnet.main.id
+  subnet_id            = aws_subnet.public.id
   key_name             = aws_key_pair.key.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2_app.name
+  user_data            = file("./userdata")
 
   vpc_security_group_ids = [
     aws_security_group.app.id
@@ -16,6 +17,24 @@ resource "aws_instance" "web" {
   associate_public_ip_address = true
 
   tags = merge(local.common_tags, {
-    Name : "${local.common_name}-app"
+    Name : "${local.common_name}-app-public"
+  })
+}
+
+resource "aws_instance" "private_app" {
+  ami                  = local.ami_id
+  instance_type        = local.instance_type
+  subnet_id            = aws_subnet.private.id
+  key_name             = aws_key_pair.key.key_name
+  iam_instance_profile = aws_iam_instance_profile.ec2_app.name
+  user_data            = file("./userdata")
+
+  vpc_security_group_ids = [
+    aws_security_group.app.id
+  ]
+  associate_public_ip_address = true
+
+  tags = merge(local.common_tags, {
+    Name : "${local.common_name}-app-private"
   })
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,13 +6,23 @@ resource "aws_vpc" "main" {
   })
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "192.168.${var.vpc_cidr_number}.0/28"
   availability_zone = var.availability_zone
 
   tags = merge(local.common_tags, {
-    Name : "${local.common_name}-${var.availability_zone}"
+    Name : "${local.common_name}-public-${var.availability_zone}"
+  })
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "192.168.${var.vpc_cidr_number}.16/28"
+  availability_zone = var.availability_zone
+
+  tags = merge(local.common_tags, {
+    Name : "${local.common_name}-private-${var.availability_zone}"
   })
 }
 
@@ -24,11 +34,11 @@ resource "aws_internet_gateway" "igw" {
   })
 }
 
-resource "aws_route_table" "r" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "${chomp(data.http.myip.body)}/32"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
@@ -37,9 +47,27 @@ resource "aws_route_table" "r" {
   })
 }
 
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.r.id
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(local.common_tags, {
+    Name : "${local.common_name}-private"
+  })
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_vpc_endpoint" "dynamodb" {
@@ -52,6 +80,6 @@ resource "aws_vpc_endpoint" "dynamodb" {
 }
 
 resource "aws_vpc_endpoint_route_table_association" "dynamodb" {
-  route_table_id  = aws_route_table.r.id
+  route_table_id  = aws_route_table.private.id
   vpc_endpoint_id = aws_vpc_endpoint.dynamodb.id
 }
